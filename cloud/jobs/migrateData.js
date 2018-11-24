bb2adc4479c6b269c447f43c3df26573cbb24a69
neimadVN <hoanglam41@gmail.com>
2018-11-24 12:47:00 +0700

    arrage code for cloud job

---
+++ cloud/jobs/migrateData.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const migrateData = {};
+
+migrateData.migrateTags = function(request) {
+    
+};
+
+migrateData.migrateProduct = function(request) {
+
+};
+
+module.exports = migrateData
--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,11 @@
+'use strict';
+
 //add your own functions here
 Parse.Cloud.define("hello", function(request, response) {
   return "world!";
 });
+
+
+// [JOB] - migrate Data
+const migrateData = require('./jobs/migrateData');
+Parse.Cloud.job("migrateTags", migrateData.migrateTags);
